d3324e4161f7771ad759b4fe78ce784f7bbce9af
Alex Avoyants <shhavel@gmail.com>
2015-01-08 13:55:31 +0200

    Allow to omit the optional arguments without errors in Rusen.notify

--- spec/lib/rusen_spec.rb
+++ spec/lib/rusen_spec.rb
@@ -26,6 +26,14 @@ describe Rusen do
       Rusen.notify(exception, request, environment, session)
     end
 
+    it 'allows to omit the optional arguments without errors' do
+      expect_any_instance_of(Rusen::Notifiers::BaseNotifier).to_not receive(:handle_notification_exception)
+      expect_any_instance_of(Rusen::Notifier).to_not receive(:warn).
+        with("Rusen: Some or all the notifiers failed to sent the notification.")
+      Rusen.settings.outputs = [:io]
+      Rusen.notify(Exception.new)
+    end
+
   end
 
 end
